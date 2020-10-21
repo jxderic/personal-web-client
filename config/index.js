47c070d60b4424ebded790274919a111c8e95bd8
jinxiaodong <jinxiaodong@hikvision.com.cn>
2020-10-21 10:33:39 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,11 @@
 /*
+ * @Descripttion: 
  * @Author: jinxiaodong
- * @Date: 2019-11-25 10:08:03
+ * @Date: 2020-09-27 19:37:12
  * @LastEditors: jinxiaodong
- * @LastEditTime: 2020-10-21 09:53:11
- * @Description: 配置文件
+ * @LastEditTime: 2020-10-21 10:33:34
  */
+
 const Config = {
   baseUrl: process.env.NODE_ENV === 'production' ? 'https://nest-api.jxderic.online/eric-api' : 'http://127.0.0.1:3008/eric-api',
   staticPath: 'resource.shirmy.me'
